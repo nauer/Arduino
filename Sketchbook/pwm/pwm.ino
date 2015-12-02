28187d50fc86cbd9fd638da000b407a2b7afb053
Norbert Auer <norbert.auer@boku.ac.at>
2015-12-02 13:17:33 +0100

    update

--- Sketchbook/pwm/pwm.ino
+++ Sketchbook/pwm/pwm.ino
@@ -6,7 +6,8 @@ void setup()
   // Set pin 13 to output mode
   pinMode(pin, OUTPUT);
   pinMode(A0, INPUT);
-  
+
+  // values from 0 to 255 (8-bit output)
   analogWrite(pin,127);
   Serial.begin(9600);
 }
