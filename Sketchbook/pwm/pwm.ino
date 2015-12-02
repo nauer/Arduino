e7fee23ebde6ff80aea5a973cc889f519bf72cbf
Norbert Auer <norbert.auer@boku.ac.at>
2015-12-02 11:41:23 +0100

    Update

---
+++ Sketchbook/pwm/pwm.ino
@@ -0,0 +1,20 @@
+// pwm.ino
+const int pin = 10; // also the SMD Led on the Arduino
+
+void setup()
+{
+  // Set pin 13 to output mode
+  pinMode(pin, OUTPUT);
+  pinMode(A0, INPUT);
+  
+  analogWrite(pin,50);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println(analogRead(A0));
+  delay(250);
+}
+
+
