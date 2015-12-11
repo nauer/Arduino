7167207e5f4c74d8450b7e3301ce90b4ef2cdad7
nauer <norbert@derauer.net>
2015-12-11 20:39:35 +0100

    Update websocket example

--- Sketchbook/websocket/server/server.py
+++ Sketchbook/websocket/server/server.py
@@ -15,13 +15,18 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("../html/front.html")
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    trig = True
     def open(self, *args):
         self.stream.set_nodelay(True)
-        
-        while 1:  # Wait forever for anything
+
+        while WebSocketHandler.trig:  # Wait forever for anything
           tdata = ser.readline()
           print(tdata)
           self.write_message(tdata)
+
+    def on_close(self):
+        WebSocketHandler.trig = False
+        print("WebSocket closed")
 
 
 
@@ -36,13 +41,13 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     # Create serial connection to Arduino
-    ser = serial.Serial('/dev/ttyACM0', 19200)
+    #ser = serial.Serial('/dev/ttyACM0', 19200)
 
     # check which port was really used
-    print("Connected to: {}".format(ser.name))
+    #print("Connected to: {}".format(ser.name))
 
     # Start reading on Arduino - send 1
-    ser.write(bytearray("1",'ascii'))
+    #ser.write(bytearray("1",'ascii'))
 
     application.listen(5005)
     tornado.ioloop.IOLoop.instance().start()
